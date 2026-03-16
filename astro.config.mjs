11784845be7f24796073186631b2a8af7829bf08
Pavlo Zoria <pavlo.zorja@gmail.com>
2026-03-16 17:39:28 +0200

    fix: GitHub Pages build and configure statch.io custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://warehouse-nonameapp.github.io',
-  base: '/8Jfhyk9Ndv1YJ7dxE5ZH3-statch-web-landing-8Txm2iwwEl',
+  site: 'https://statch.io',
+  base: '/',
   integrations: [react()],
 
   vite: {
